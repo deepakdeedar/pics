5f91579c8a9f27abf5b1a03b82388aa4db6fa867
Deepak Deedar Singla <47693198+deepakdeedar@users.noreply.github.com>
2020-09-07 22:27:14 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  var app = MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Let's see some Images!"),
+      ),
+    )
+  );
+
+  runApp(app);
+}
