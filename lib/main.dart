fe563a19a1137018bab520bbf3df9a95fcb28c23
Deepak Deedar Singla <47693198+deepakdeedar@users.noreply.github.com>
2020-09-07 22:46:50 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 void main() {
   var app = MaterialApp(
     home: Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        child: Icon(Icons.add),
+      ),
       appBar: AppBar(
         title: Text("Let's see some Images!"),
       ),
